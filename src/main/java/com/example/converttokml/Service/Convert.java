37df18ade996d579f359e4836ee6629fcf0e2fca
Quang <pokiwarquanden4@gmail.com>
2023-03-14 09:57:49 +0700

    update

--- src/main/java/com/example/converttokml/Service/Convert.java
+++ src/main/java/com/example/converttokml/Service/Convert.java
@@ -9,10 +9,7 @@ import org.w3c.dom.Element;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
@@ -31,38 +28,120 @@ public class Convert {
     private ArrayList<String> headers;
     private ArrayList<ArrayList<Object>> values;
     public void createFile(String fileName) {
-        String xmlFilePath = "C:/Users/Admin/Desktop/IT/ConvertToKML/KmlFile/" + fileName + ".xml";
+        String kmlPath = "C:/Users/Admin/Desktop/IT/ConvertToKML/KmlFile/" + fileName + ".kml";
 
         try {
             DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
-
             DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
-
             Document document = documentBuilder.newDocument();
 
             // root element
-            Element root = document.createElement("root");
-            document.appendChild(root);
+            Element kml = document.createElement("kml");
+            kml.setAttribute("xmlns", "http://earth.google.com/kml/2.0");
+            kml.setAttribute("xmlns:gx", "http://www.google.com/kml/ext/2.2");
+            kml.setAttribute("xmlns:kml", "http://www.opengis.net/kml/2.2");
+            kml.setAttribute("xmlns:atom", "http://www.w3.org/2005/Atom");
+            document.appendChild(kml);
+
+            // doc element
+            Element docs = document.createElement("Document");
+            kml.appendChild(docs);
+
+            // name
+            Element name = document.createElement("name");
+            name.appendChild(document.createTextNode(fileName));
+            docs.appendChild(name);
+
+            // open
+            Element open = document.createElement("open");
+            open.appendChild(document.createTextNode("1"));
+            docs.appendChild(open);
+
+            // Schema
+            Element schema = document.createElement("Schema");
+            schema.setAttribute("name", fileName);
+            schema.setAttribute("id", "S_" + fileName + "_SSSIIDDII");
+            docs.appendChild(schema);
 
             for (int i=0; i<headers.size(); i++){
-                System.out.println(headers.get(i));
-                Element input = document.createElement(headers.get(i));
-                for (ArrayList<Object> value : values){
-                    input.appendChild(document.createTextNode(String.valueOf(value.get(i))));
+                Element simpleField = document.createElement("SimpleField");
+                simpleField.setAttribute("id", "S_pge_SSSIIDDII");
+
+                Element displayName = document.createElement("displayName");
+                displayName.appendChild(document.createTextNode(headers.get(i)));
+                simpleField.appendChild(displayName);
+
+                schema.appendChild(simpleField);
+            }
+
+            //Folder
+            Element folder = document.createElement("Folder");
+            docs.appendChild(folder);
+
+            //Folder -> name
+            Element folderName = document.createElement("name");
+            folderName.appendChild(document.createTextNode(fileName));
+            folder.appendChild(folderName);
+
+            //Placemark
+            for(int i=0; i<values.size() ; i++){
+                String longitud = "";
+                String latitud = "";
+
+                Element placemark = document.createElement("Placemark");
+                folder.appendChild(placemark);
+
+                Element extendedData = document.createElement("ExtendedData");
+                placemark.appendChild(extendedData);
+
+                Element schemaData = document.createElement("SchemaData");
+                schemaData.setAttribute("schemaUrl" , "#S_" + fileName + "_SSSIIDDII");
+                extendedData.appendChild(schemaData);
+
+                Element point = document.createElement("Point");
+                placemark.appendChild(point);
+
+                Element coordinates = document.createElement("coordinates");
+
+
+                for (int j=0 ; j<values.get(i).size() ; j++){
+
+
+                    Element simpleData = document.createElement("SimpleData");
+                    simpleData.setAttribute("name", headers.get(j));
+                    simpleData.appendChild(document.createTextNode(String.valueOf(values.get(i).get(j))));
+                    extendedData.appendChild(simpleData);
+
+                    if(headers.get(j).equalsIgnoreCase("Longitud")){
+                        longitud = String.valueOf(values.get(i).get(j));
+                        System.out.println(longitud);
+                        System.out.println(longitud.length());
+                    }
+                    if(headers.get(j).equalsIgnoreCase("Latitud")){
+                        latitud = String.valueOf(values.get(i).get(j));
+                        System.out.println(latitud);
+                        System.out.println(latitud.length());
+                    }
+                    if(longitud.length() != 0 && latitud.length() != 0){
+                        System.out.println("ressult");
+                        coordinates.appendChild(document.createTextNode(longitud + "," + latitud + ",0"));
+                        longitud = "";
+                        latitud = "";
+                        point.appendChild(coordinates);
+                    }
                 }
-                root.appendChild(input);
             }
 
             // create the xml file
             //transform the DOM Object to an XML File
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+
             DOMSource domSource = new DOMSource(document);
-            StreamResult streamResult = new StreamResult(new File(xmlFilePath));
-
+            StreamResult streamResult = new StreamResult(new File(kmlPath));
             transformer.transform(domSource, streamResult);
-
-            System.out.println("Done creating XML File");
 
         } catch (ParserConfigurationException pce) {
             pce.printStackTrace();
